c362a8fd323c02f8e76aa21c5479a3cbcdbfeea0
xhan469 <xhan469@aucklanduni.ac.nz>
2023-09-11 19:15:53 +1200

    change file structure

--- sandSim.js
+++ sandSim.js
@@ -5,40 +5,95 @@ const gridWidth = 200;  // Change for finer granularity
 const gridHeight = 150; // Change for finer granularity
 const cellSize = canvas.width / gridWidth;
 
-let grid = Array(gridHeight).fill().map(() => Array(gridWidth).fill(null));
+let stoneIdCounter = 0;
 
+let grid = Array(gridHeight).fill().map(() => Array(gridWidth).fill(null));
 
 const elements = {
     sand: {
-        color: "#FFD700",   
-        //density: 0.7, gravity: 0.8, slip: 0, slide: 0.8, scatter: 0,
+        color: "#FFD700",
+        behavior: [],
+    },
+    soil: {
+        color: "#452c1b",
+        behavior: [],
+    },
+    stone: {
+        color: "#211811",
         behavior: [],
     },
 };
 
-let elementId = 0;
-for(const elementName in elements){
-    elements[elementName].id = elementId++;
-}
+elements.stone.behavior.push(function (y, x, grid) {
 
-//let currentParticleType = 'sand';
-
-
-
-
-elements.sand.behavior.push(function(y, x, grid) {
-    // Sand behavior logic goes here, based on the extracted updateGrid function
-    if (grid[y + 1][x] === null) {
-        // Move sand down
-        grid[y + 1][x] = 'sand';
-        grid[y][x] = null;
-    } else if (grid[y + 1][x] === 'water') {
-        // If there's water below the sand, swap the two
-        grid[y + 1][x] = 'sand';
-        grid[y][x] = 'water';
-    }
-    // ... rest of the sand behavior ...
 });
+
+elements.soil.behavior.push(function (y, x, grid) {
+
+    if (grid[y + 1][x] === null) {
+        // If the bottom is empty, let the dirt move downward
+        grid[y + 1][x] = 'soil';
+        grid[y][x] = null;
+    } else {
+        // If the bottom is not empty, try to let the dirt slide to the sides
+        let leftX = x - 1;
+        let rightX = x + 1;
+
+        if (leftX >= 0 && rightX < gridWidth) {
+            let leftHeight = 0;
+            let rightHeight = 0;
+
+            // Calculate the height of the left and right sides
+            while (leftX >= 0 && grid[y + 1][leftX] === null) {
+                leftHeight++;
+                leftX--;
+            }
+            while (rightX < gridWidth && grid[y + 1][rightX] === null) {
+                rightHeight++;
+                rightX++;
+            }
+
+            // If the height of the left side is greater than or equal to 3 or the height of the right side is greater than or equal to 3, let the clods slide in both directions
+            if (leftHeight >= 3 || rightHeight >= 3) {
+                if (leftHeight >= rightHeight) {
+                    grid[y + 1][x - leftHeight] = 'soil';
+                    grid[y][x] = null;
+                } else {
+                    grid[y + 1][x + rightHeight] = 'soil';
+                    grid[y][x] = null;
+                }
+            }
+        }
+    }
+});
+
+
+function drawSandAutomatically() {
+
+    for (let y = gridHeight - 1; y >= gridHeight*0.6; y--) {
+        for (let x = 0; x < gridWidth; x++) {
+
+            if (grid[y][x] === null) {
+                grid[y][x] = 'soil';
+
+                if (Math.random() < 0.005) {
+                    const stoneSizeX = Math.floor(Math.random() * 2) + 4;
+                    const stoneSizeY = Math.floor(Math.random() * 2) + 4;
+                    const stoneId = `stone-${stoneIdCounter++}`;
+
+                    for (let i = 0; i < stoneSizeX; i++) {
+                        for (let j = 0; j < stoneSizeY; j++) {
+                            const stoneX = x + i;
+                            const stoneY = y - j;
+                            grid[stoneY][stoneX] = stoneId;
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
 
 function updateGrid() {
     for (let y = gridHeight - 2; y >= 0; y--) {
@@ -52,32 +107,6 @@ function updateGrid() {
         }
     }
 }
-            
-
-
-function drawGrid() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for (let y = 0; y < gridHeight; y++) {
-        for (let x = 0; x < gridWidth; x++) {
-            if (grid[y][x] in elements){
-                ctx.fillStyle = elements[grid[y][x]].color; 
-                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-            }
-        }
-    }
-
-}
-
-/*
-canvas.addEventListener('mousedown', (event) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = Math.floor((event.clientX - rect.left) / cellSize);
-    const y = Math.floor((event.clientY - rect.top) / cellSize);
-    grid[y][x] = currentParticleType;
-});
-*/
-
 
 
 function loop() {
@@ -86,21 +115,25 @@ function loop() {
     requestAnimationFrame(loop);
 }
 
+function drawGrid() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    for (let y = 0; y < gridHeight; y++) {
+        for (let x = 0; x < gridWidth; x++) {
+            const element = grid[y][x];
 
-window.addEventListener('load', function() {
-    // Logic to draw sand on the canvas automatically
-    // This is a placeholder; the actual logic will depend on the structure of the JS code.
+            if (element in elements) {
+                ctx.fillStyle = elements[grid[y][x]].color;
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            } else if (element && element.startsWith('stone-')) {
+                ctx.fillStyle = elements.stone.color;
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    }
+}
+
+window.addEventListener('load', function () {
     loop();
     drawSandAutomatically();
 });
-
-function drawSandAutomatically() {
-    // Logic to draw sand on the canvas
-    // This is a placeholder; the actual logic will depend on the structure of the JS code.
-    // For example:
-    
-    grid[20][20] = 'sand'; // Fill the grid with sand
-
-    // Call any other functions required to render the grid on the canvas.
-}
