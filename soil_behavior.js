03f76c21c8f70af733fbad36c63c8f03a128747c
jessie695 <yil695@aucklanduni.ac.nz>
2023-10-10 01:23:40 +1300

    update soil and aggregate files

---
+++ soil_behavior.js
@@ -0,0 +1,37 @@
+export function soilBehavior(y, x, grid) {
+    const gridHeight = grid.length;
+    const gridWidth = grid[0].length;
+
+    if (grid[y + 1][x] === null) {
+        // If the bottom is empty, let the dirt move downward
+        grid[y + 1][x] = 'soil';
+        grid[y][x] = null;
+    } else {
+        // If the bottom is not empty, try to let the dirt slide to the sides
+        let leftX = x - 1;
+        let rightX = x + 1;
+        if (leftX >= 0 && rightX < gridWidth) {
+            let leftHeight = 0;
+            let rightHeight = 0;
+            // Calculate the height of the left and right sides
+            while (leftX >= 0 && grid[y + 1][leftX] === null) {
+                leftHeight++;
+                leftX--;
+            }
+            while (rightX < gridWidth && grid[y + 1][rightX] === null) {
+                rightHeight++;
+                rightX++;
+            }
+            // If the height of the left side is greater than or equal to 3 or the height of the right side is greater than or equal to 3, let the clods slide in both directions
+            if (leftHeight >= 3 || rightHeight >= 3) {
+                if (leftHeight >= rightHeight) {
+                    grid[y + 1][x - leftHeight] = 'soil';
+                    grid[y][x] = null;
+                } else {
+                    grid[y + 1][x + rightHeight] = 'soil';
+                    grid[y][x] = null;
+                }
+            }
+        }
+    }
+}
