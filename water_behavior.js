ed84d69caf07adf5e8d0d6d54da8881a2e675f0b
xhan469 <xhan469@aucklanduni.ac.nz>
2023-10-11 01:14:15 +1300

    adjust water interaction with the soil

--- water_behavior.js
+++ water_behavior.js
@@ -1,11 +1,23 @@
-
+import { elements } from "./sandSim.js";
 
 export function waterBehavior(y, x, grid, gridHeight) {
     // Check for an empty space below and move water down
     if (y + 1 < gridHeight && grid[y + 1][x] === null) {
         grid[y + 1][x] = 'water';
         grid[y][x] = null;
-    } else {
+    } else if( y + 1 < gridHeight && grid[y + 1][x] === 'soil') {
+        if (elements.soil.soilAlpha[(y+1) + "," + x] <= 0.7){
+            if (grid[y + 1][x] = 'soil'){
+                grid[y + 1][x] = "waterInSoil";
+                
+            }
+        }
+        else{
+            grid[y][x] = 'water';
+        }
+        
+    }
+    else {
         grid[y][x] = null;
     }
 }
