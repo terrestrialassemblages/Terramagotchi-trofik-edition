a68f013299039a033855214ab8e472bbd3675ad6
ryam562 <ryam562@aucklanduni.ac.nz>
2023-10-24 00:55:28 +1300

    Remove sensitive information

---
+++ firebase_config.js
@@ -0,0 +1,13 @@
+// Change the following according to your Firebase project configuration
+const FIREBASE_CONFIG = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_DOMAIN",
+    databaseURL: "YOUR_APP_URL",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_APP.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+export default FIREBASE_CONFIG;
